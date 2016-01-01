d1950b56f8393c3abe31a313ce804547916c027a
prasanna-hr <prasanna.hr@gmail.com>
2016-01-01 19:47:28 +0000

    fix errors

--- program/models.py
+++ program/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Program(models.Model):
-       title = models.CharField(max_length=100)
+   title = models.CharField(max_length=100)
    event_type = models.CharField(max_length=100, default='No Information')
    description = models.CharField(max_length=1024)
    entry_fee = models.CharField(max_length=512, default='No Information')
--- program/urls.py
+++ program/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from program import views
 
@@ -9,4 +11,4 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^programs/$', views.ProgramList.as_view()),
     url(r'^programs_detail/(?P<pk>[0-9]+)/$', views.ProgramDetail.as_view()),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
